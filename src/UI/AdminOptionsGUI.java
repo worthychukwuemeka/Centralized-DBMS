d97c7483bf10817be727f519ca281a255d3de731
worthychukwuemeka <worthy.chukwuemeka@stu.cu.edu.ng>
2023-08-11 19:47:32 +0100

    First commit, 212 project.

---
+++ src/UI/AdminOptionsGUI.java
@@ -0,0 +1,264 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminOptionsGUI extends JFrame {
+    private String adminName;
+
+    public AdminOptionsGUI(String adminName) {
+        this.adminName = adminName;
+
+        setTitle("Admin Dashboard");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        getContentPane().setBackground(new Color(255, 182, 193));
+
+        Font labelFont = new Font("Arial", Font.BOLD, 16);
+        Font buttonFont = new Font("Arial", Font.PLAIN, 14);
+
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        JLabel headerLabel = new JLabel("What would you like to do today?");
+        headerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        headerLabel.setFont(labelFont);
+        add(headerLabel);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 10));
+        buttonPanel.setBackground(new Color(255, 182, 193));
+
+        JButton viewExistingDataButton = new JButton("View Existing User Data");
+        JButton registerNewUserButton = new JButton("Register New User");
+        viewExistingDataButton.setFont(buttonFont);
+        registerNewUserButton.setFont(buttonFont);
+        viewExistingDataButton.setBackground(Color.decode("#EEEEEE"));
+        registerNewUserButton.setBackground(Color.decode("#EEEEEE"));
+        viewExistingDataButton.setForeground(Color.decode("#222222"));
+        registerNewUserButton.setForeground(Color.decode("#222222"));
+
+        buttonPanel.add(viewExistingDataButton);
+        buttonPanel.add(registerNewUserButton);
+
+        add(buttonPanel);
+
+        viewExistingDataButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                viewExistingUserData();
+            }
+        });
+
+        registerNewUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openRegistrationForm();
+            }
+        });
+
+        setVisible(true);
+    }
+
+
+    private void viewExistingUserData() {
+        try {
+            File csvFile = new File("C:\\Users\\worth\\IdeaProjects\\project-work\\existing_user_data.csv");
+            Desktop.getDesktop().open(csvFile);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Failed to open CSV file: " + ex.getMessage());
+        }
+    }
+
+
+    private List<String[]> readUserDataFromCSV() {
+        List<String[]> userData = new ArrayList<>();
+
+        String filePath = "C:\\Users\\worth\\IdeaProjects\\project-work\\existing_user_data.csv";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] rowData = line.split(",");
+                userData.add(rowData);
+            }
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "Failed to read user data: " + ex.getMessage());
+        }
+
+        return userData;
+    }
+
+
+    private void openRegistrationForm() {
+        JFrame registrationFormFrame = new JFrame("Register New User");
+        registrationFormFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        registrationFormFrame.setSize(400, 500);
+        registrationFormFrame.setLocationRelativeTo(null);
+        registrationFormFrame.setLayout(new GridLayout(13, 2));
+        registrationFormFrame.getContentPane().setBackground(new Color(255, 182, 193));
+
+        Font labelFont = new Font("Arial", Font.BOLD, 14);
+
+        JLabel fullNameLabel = new JLabel("Full Name:");
+        fullNameLabel.setFont(labelFont);
+
+        JTextField fullNameField = new JTextField();
+        fullNameField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel ageLabel = new JLabel("Age:");
+        ageLabel.setFont(labelFont);
+
+        JTextField ageField = new JTextField();
+        ageField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel stateLabel = new JLabel("State of Origin:");
+        stateLabel.setFont(labelFont);
+
+        JTextField stateField = new JTextField();
+        stateField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel lgaLabel = new JLabel("LGA:");
+        lgaLabel.setFont(labelFont);
+
+        JTextField lgaField = new JTextField();
+        lgaField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel phoneLabel = new JLabel("Phone Number:");
+        phoneLabel.setFont(labelFont);
+
+        JTextField phoneField = new JTextField();
+        phoneField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel emailLabel = new JLabel("Email:");
+        emailLabel.setFont(labelFont);
+
+        JTextField emailField = new JTextField();
+        emailField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel ninLabel = new JLabel("NIN:");
+        ninLabel.setFont(labelFont);
+
+        JTextField ninField = new JTextField();
+        ninField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel nextOfKinLabel = new JLabel("Next of Kin:");
+        nextOfKinLabel.setFont(labelFont);
+
+        JTextField nextOfKinField = new JTextField();
+        nextOfKinField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel maritalStatusLabel = new JLabel("Marital Status:");
+        maritalStatusLabel.setFont(labelFont);
+
+        JTextField maritalStatusField = new JTextField();
+        maritalStatusField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel genotypeLabel = new JLabel("Genotype:");
+        genotypeLabel.setFont(labelFont);
+
+        JTextField genotypeField = new JTextField();
+        genotypeField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel bloodGroupLabel = new JLabel("Blood Group:");
+        bloodGroupLabel.setFont(labelFont);
+
+        JTextField bloodGroupField = new JTextField();
+        bloodGroupField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JLabel genderLabel = new JLabel("Gender:");
+        genderLabel.setFont(labelFont);
+
+        JTextField genderField = new JTextField();
+        genderField.setFont(new Font("Arial", Font.PLAIN, 12));
+
+        JButton submitButton = new JButton("Submit");
+        submitButton.setFont(new Font("Arial", Font.BOLD, 14));
+        submitButton.setBackground(Color.decode("#EEEEEE"));
+        submitButton.setForeground(Color.decode("#222222"));
+
+        registrationFormFrame.add(fullNameLabel);
+        registrationFormFrame.add(fullNameField);
+        registrationFormFrame.add(ageLabel);
+        registrationFormFrame.add(ageField);
+        registrationFormFrame.add(stateLabel);
+        registrationFormFrame.add(stateField);
+        registrationFormFrame.add(lgaLabel);
+        registrationFormFrame.add(lgaField);
+        registrationFormFrame.add(phoneLabel);
+        registrationFormFrame.add(phoneField);
+        registrationFormFrame.add(emailLabel);
+        registrationFormFrame.add(emailField);
+        registrationFormFrame.add(ninLabel);
+        registrationFormFrame.add(ninField);
+        registrationFormFrame.add(nextOfKinLabel);
+        registrationFormFrame.add(nextOfKinField);
+        registrationFormFrame.add(maritalStatusLabel);
+        registrationFormFrame.add(maritalStatusField);
+        registrationFormFrame.add(genotypeLabel);
+        registrationFormFrame.add(genotypeField);
+        registrationFormFrame.add(bloodGroupLabel);
+        registrationFormFrame.add(bloodGroupField);
+        registrationFormFrame.add(genderLabel);
+        registrationFormFrame.add(genderField);
+        registrationFormFrame.add(submitButton);
+
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String fullName = fullNameField.getText();
+                String age = ageField.getText();
+                String phone = phoneField.getText();
+                String email = emailField.getText();
+                String nin = ninField.getText();
+                String state = stateField.getText();
+                String lga = lgaField.getText();
+                String nextOfKin = nextOfKinField.getText();
+                String maritalStatus = maritalStatusField.getText();
+                String genotype = genotypeField.getText();
+                String bloodGroup = bloodGroupField.getText();
+                String gender = genderField.getText();
+
+                String[] rowData = {fullName, age, phone, email, nin, state, lga, nextOfKin, maritalStatus, genotype, bloodGroup, gender};
+
+                String filePath = "C:\\Users\\worth\\IdeaProjects\\project-work\\existing_user_data.csv";
+
+                try {
+                    FileWriter fileWriter = new FileWriter(filePath, true);
+                    PrintWriter printWriter = new PrintWriter(fileWriter);
+
+                    printWriter.println(String.join(",", rowData));
+                    printWriter.close();
+
+                    JOptionPane.showMessageDialog(null, "User registered successfully!");
+
+                    fullNameField.setText("");
+                    ageField.setText("");
+                    // ... (clear other fields)
+                } catch (IOException ex) {
+                    JOptionPane.showMessageDialog(null, "Failed to register user: " + ex.getMessage());
+                }
+            }
+        });
+
+        registrationFormFrame.setVisible(true);
+    }
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                String fullName = "John Doe";
+                AdminOptionsGUI optionsGUI = new AdminOptionsGUI(fullName);
+                optionsGUI.setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/UI/WelcomeWindowGUI.java
@@ -0,0 +1,159 @@
+package UI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WelcomeWindowGUI extends JFrame {
+    private JComboBox<String> orgComboBox;
+
+    public WelcomeWindowGUI() {
+        setTitle("Welcome to the Centralized Database Management System");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 300);
+        setLocationRelativeTo(null);
+        getContentPane().setBackground(new Color(255, 182, 193));
+
+        Font labelFont = new Font("Arial", Font.BOLD, 16);
+        Font buttonFont = new Font("Arial", Font.PLAIN, 14);
+        Font comboFont = new Font("Arial", Font.PLAIN, 14);
+
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        JLabel headerLabel = new JLabel("Welcome to the Centralized Database Management System");
+        headerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        headerLabel.setFont(labelFont);
+        add(headerLabel);
+
+        JLabel paragraphLabel = new JLabel("Select an organization to view its records");
+        paragraphLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        paragraphLabel.setFont(labelFont);
+        add(paragraphLabel);
+
+        orgComboBox = new JComboBox<>(new String[]{"NIMC", "NIS", "INC", "FRSC"});
+        orgComboBox.setMaximumSize(new Dimension(200, 30));
+        orgComboBox.setAlignmentX(Component.CENTER_ALIGNMENT);
+        orgComboBox.setFont(comboFont);
+        add(orgComboBox);
+
+        add(Box.createVerticalGlue());
+
+        JButton nextButton = new JButton("Next");
+        nextButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        nextButton.setFont(buttonFont);
+        nextButton.setBackground(Color.decode("#EEEEEE"));
+        nextButton.setForeground(Color.decode("#222222"));
+        add(nextButton);
+
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                openLoginWindow();
+            }
+        });
+
+        setVisible(true);
+    }
+
+    private void openLoginWindow() {
+        String selectedOrg = (String) orgComboBox.getSelectedItem();
+
+        JFrame loginFrame = new JFrame("Login to access user data");
+        loginFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        loginFrame.setSize(400, 300);
+        loginFrame.setLocationRelativeTo(null);
+        loginFrame.getContentPane().setBackground(new Color(255, 182, 193)); // Baby Pink Background
+
+        Font labelFont = new Font("Arial", Font.BOLD, 14);
+        Font fieldFont = new Font("Arial", Font.PLAIN, 12);
+        Font buttonFont = new Font("Arial", Font.PLAIN, 14);
+
+        loginFrame.setLayout(new BoxLayout(loginFrame.getContentPane(), BoxLayout.Y_AXIS));
+
+        JLabel headerLabel = new JLabel(selectedOrg + " Login to access user data");
+        headerLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        headerLabel.setFont(labelFont);
+        loginFrame.add(headerLabel);
+
+        JLabel usernameLabel = new JLabel("Admin Username:");
+        usernameLabel.setFont(labelFont);
+
+        JTextField usernameField = new JTextField();
+        usernameField.setFont(fieldFont);
+        usernameField.setMaximumSize(new Dimension(200, 30));
+
+        JPanel usernamePanel = new JPanel();
+        usernamePanel.setLayout(new BoxLayout(usernamePanel, BoxLayout.X_AXIS));
+        usernamePanel.add(usernameLabel);
+        usernamePanel.add(usernameField);
+
+        loginFrame.add(usernamePanel);
+
+        JLabel passwordLabel = new JLabel("Admin Password:");
+        passwordLabel.setFont(labelFont);
+
+        JPasswordField passwordField = new JPasswordField();
+        passwordField.setFont(fieldFont);
+        passwordField.setMaximumSize(new Dimension(200, 30));
+
+        JPanel passwordPanel = new JPanel();
+        passwordPanel.setLayout(new BoxLayout(passwordPanel, BoxLayout.X_AXIS));
+        passwordPanel.add(passwordLabel);
+        passwordPanel.add(passwordField);
+
+        loginFrame.add(passwordPanel);
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        loginButton.setFont(buttonFont);
+        loginButton.setBackground(Color.decode("#EFEFEF"));
+        loginButton.setForeground(Color.decode("#222222"));
+
+        loginFrame.add(loginButton);
+
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String password = new String(passwordField.getPassword());
+
+                if (isValidLogin(username, password)) {
+                    loginFrame.dispose();
+                    openAdminOptions(selectedOrg);
+                } else {
+                    JOptionPane.showMessageDialog(loginFrame, "Invalid credentials. Please try again.");
+                }
+            }
+        });
+
+        loginFrame.setVisible(true);
+    }
+
+    private boolean isValidLogin(String username, String password) {
+        if (username.equals("johnpatrick") && password.equals("password1")) {
+            return true;
+        } else if (username.equals("sarahgbenga") && password.equals("password2")) {
+            return true;
+        } else if (username.equals("fatimamusa") && password.equals("password3")) {
+            return true;
+        } else if (username.equals("philipokocha") && password.equals("password4")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void openAdminOptions(String organization) {
+        new AdminOptionsGUI(organization);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new WelcomeWindowGUI();
+            }
+        });
+    }
+}
